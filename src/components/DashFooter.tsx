b538fa12f0284419606226d9492d978a1576d56b
william chang <coooo77@hotmail.comcls>
2023-04-11 23:17:46 +0800

    feat: basic route

---
+++ src/components/DashFooter.tsx
@@ -0,0 +1,27 @@
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const DashFooter = () => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const onGoHomeClicked = () => navigate('/dash')
+
+  const goHomeButton =
+    pathname !== '/dash' ? (
+      <button className="dash-footer__button icon-button" title="Home" onClick={onGoHomeClicked}>
+        <FontAwesomeIcon icon={faHouse} />
+      </button>
+    ) : null
+
+  return (
+    <footer className="dash-footer">
+      {goHomeButton}
+      <p>Current User:</p>
+      <p>Status:</p>
+    </footer>
+  )
+}
+
+export default DashFooter
